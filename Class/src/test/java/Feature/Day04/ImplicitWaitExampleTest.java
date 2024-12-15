fa5736444e3202a69359c093b4736efeb82b2230
vuthanhlan <thanhlan050109@gmail.com>
2024-12-15 15:58:16 +0700

    wait

---
+++ Class/src/test/java/Feature/Day04/ImplicitWaitExampleTest.java
@@ -0,0 +1,31 @@
+package Feature.Day04;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ImplicitWaitExampleTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://saucelabs.com/request-demo");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        //Chờ cho phần tử Email xuất hiện
+        WebElement firstNameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Email")));
+        firstNameField.sendKeys("test@gmail.com");
+
+        //Cờ cho nút submit có thể nhấp
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(@type,'sub')]")));
+        submitButton.click();
+
+//        driver.quit();
+    }
+}
